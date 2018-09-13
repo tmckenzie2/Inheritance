6c6455ab1cbfe9b8bba28a7c093da92d344a28ce
McKenzie, Thomas Patrick <tmckenzie@bkba-25029al.gonzaga.edu>
2018-09-13 10:11:53 -0700

    Initial Commit

---
+++ Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance
+//
+//  Created by McKenzie, Thomas Patrick on 9/13/18.
+//  Copyright © 2018 McKenzie, Thomas Patrick. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
